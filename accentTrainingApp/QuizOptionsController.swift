8047138be5860714639cb001e39ebf32fe815f0e
Mujavid Bukhari <sirmuj@gmail.com>
2016-03-22 16:19:41 +0000

    Adjust all buttons so that they autosize to screen

--- accentTrainingApp/QuizOptionsController.swift
+++ accentTrainingApp/QuizOptionsController.swift
@@ -34,21 +34,21 @@ class QuizOptionsController: CustomViewController{
 	
     func getLengthOptions(){
         displayLabel("Choose quiz length", textColor: UIColor(red: 90/255, green: 158/255, blue: 1, alpha: 1))
-		displayButtons(lengthOptions, textColor: appColors["practice"]!, nextFunction: "getAccentOptions:")
+		displaySingleColumnButtons(lengthOptions, textColor: appColors["practice"]!, nextFunction: "getAccentOptions:")
     }
 	
     func getAccentOptions(sender: CustomButton){
         self.quizOptions.setLength(sender.currentTitle!)
         removeViews(1)
         displayLabel("Choose an accent", textColor: UIColor(red: 90/255, green: 158/255, blue: 1, alpha: 1))
-        displayButtons(accentOptions, textColor: appColors["practice"]!, nextFunction:"getSpeakerOptions:")
+        displayTwoColumnButtons(accentOptions, textColor: appColors["practice"]!, nextFunction:"getSpeakerOptions:")
     }
     
     func getSpeakerOptions(sender: CustomButton){
         self.quizOptions.setAccent(sender.currentTitle!)
         removeViews(1)
         displayLabel("Choose a speaker", textColor: UIColor(red: 90/255, green: 158/255, blue: 1, alpha: 1))
-        displayButtons(speakerOptions[quizOptions.getQuizAccent()]!, textColor: appColors["practice"]!, nextFunction: "moveToQuestionView:")
+        displayTwoColumnButtons(speakerOptions[quizOptions.getQuizAccent()]!, textColor: appColors["practice"]!, nextFunction: "moveToQuestionView:")
     }
     
     func moveToQuestionView(sender:CustomButton){
@@ -59,7 +59,38 @@ class QuizOptionsController: CustomViewController{
         }
     }
 	
-	func displayButtons(buttonLabelSet: [String], textColor: UIColor, nextFunction: Selector){
+	func displaySingleColumnButtons(buttonLabelSet: [String], textColor: UIColor, nextFunction: Selector){
+		var posX: Int
+		var posY: Int
+		var counter = 1
+		
+		let viewHeight = Float(self.view.frame.height)
+		let viewWidth = Float(self.view.frame.width)
+		let gutterSize = Int(viewWidth / 18.75)
+		let buttonWidth = Int(viewWidth) - (2 * gutterSize)
+		posX = gutterSize;
+		
+		let buttonHeight = Int(((Int(viewHeight * 0.5) / (buttonLabelSet.count)) - gutterSize))
+		
+		for label in buttonLabelSet {
+			
+			posY = Int(80 + (counter * (buttonHeight + gutterSize)))
+			
+			let customButton = CustomButton(
+				frame: CGRect(x: posX, y: posY, width: buttonWidth, height: buttonHeight)
+			)
+			customButton.setTitleColor(textColor, forState: .Normal)
+			customButton.setTitle(label, forState: UIControlState.Normal)
+			customButton.titleLabel!.font = UIFont(name: "Arial", size: CGFloat(viewWidth / 14.4))
+			customButton.addTarget(self, action: nextFunction, forControlEvents: .TouchUpInside)
+			customButton.backgroundColor = appColors["white"]
+			self.view.addSubview(customButton)
+			customButton.tag = 1
+			counter = counter + 1
+		}
+	}
+	
+	func displayTwoColumnButtons(buttonLabelSet: [String], textColor: UIColor, nextFunction: Selector){
 		
 		var posX: Int
 		var posY: Int
@@ -68,8 +99,8 @@ class QuizOptionsController: CustomViewController{
 		//select (x, y, width, height) based on actual view dimensions
 		let viewHeight = Float(self.view.frame.height);
 		let viewWidth = Float(self.view.frame.width);
-		let gutterWidth: Float = 20;
-		let buttonWidth: Float = (viewWidth - (3 * gutterWidth))/2
+		let gutterWidth = viewWidth / 18.75;
+		let buttonWidth = (viewWidth - (3 * gutterWidth))/2
 		
 		//get 75% of height, remove gutter space and divide remaining area by 3
 		let buttonHeight = (((viewHeight) * 0.75) - (4 * gutterWidth)) / 3
@@ -84,7 +115,7 @@ class QuizOptionsController: CustomViewController{
 			)
 			customButton.setTitleColor(textColor, forState: .Normal)
 			customButton.setTitle(label, forState: UIControlState.Normal)
-			customButton.titleLabel!.font = UIFont(name: "Arial", size: 23)
+			customButton.titleLabel!.font = UIFont(name: "Arial", size: CGFloat(viewWidth / 14.4))
 			customButton.addTarget(self, action: nextFunction, forControlEvents: .TouchUpInside)
 			customButton.backgroundColor = appColors["white"]
 			self.view.addSubview(customButton)
